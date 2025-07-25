5bf18903a8813837bfde3cc3b60e698472920f5d
Ojhashish07personal <ashishkumarojha07@gmail.com>
2025-07-25 11:04:11 +0530

    chore: Trigger full redeployment after manual cleanup

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -3,12 +3,20 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0" # Specify a compatible AWS provider version
+      version = "~> 5.0"
     }
   }
-  required_version = ">= 1.0.0" # Specify a compatible Terraform version
+  required_version = ">= 1.0.0"
+
+  # Configure S3 backend for Terraform state
+  backend "s3" {
+    bucket         = "ojhashish-devops-project-terraform-state-2025-unique" # <--- REPLACE with YOUR UNIQUE BUCKET NAME
+    key            = "devops-two-tier-app.tfstate" # Name of the state file in S3
+    region         = "ap-south-1" # Your chosen region
+    encrypt        = true # Encrypt the state file in S3
+  }
 }
 
 provider "aws" {
-  region = var.aws_region # AWS region will be set by a variable
+  region = var.aws_region
 }
